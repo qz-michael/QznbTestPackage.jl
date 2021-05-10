7e58124d1743bb609ea96761c6dea47f8801d578
Documenter.jl <documenter@juliadocs.github.io>
2021-05-10 05:44:41 +0000

    build based on 2ef2fd6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QznbTestPackage","category":"page"},{"location":"#QznbTestPackage","page":"Home","title":"QznbTestPackage","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QznbTestPackage.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QznbTestPackage]","category":"page"},{"location":"#QznbTestPackage.hello-Tuple{String}","page":"Home","title":"QznbTestPackage.hello","text":"hello(who::String)\n\nReturn \"Hello, who\".\n\n\n\n\n\n","category":"method"}]
+}
